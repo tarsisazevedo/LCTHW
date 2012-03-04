9ec0ce82b1a2dccfc626a352f318aa71783c7448
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2012-03-03 21:44:05 -0300

    apagar testes

--- chapter 2/ex6.c
+++ chapter 2/ex6.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     int hexa = 0x23;
     int octa = 012;
 
-    printf("You are %ld miles away.\n", distance);
+    printf("You are %d miles away.\n", distance);
     printf("You have %.3f levels of power.\n", power);
     printf("You have %040.2f awesome super power\n", super_power);
     printf("I have an initial %c.\n", initial);
